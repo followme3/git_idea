3cef6fbbadb62b003dffb0a46e1dfd5730315c30
lpb <1075547257@qq.com>
2022-04-01 16:47:52 +0800

    test 分支第2次提交

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -2,5 +2,7 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git!!");
         System.out.println("hello git test 分支");
+        System.out.println("这是GitHub的第一次提交");
+
     }
 }
